b577ece74d2447d25f3dbe899720dd258899310a
shotaro-ooe <shoooe97@gmail.com>
2021-09-11 19:46:51 +0900

    ステージ作成

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    Vector3 startPositon;
+
+    [SerializeField] float amlitude; //移動量パラメータ
+    [SerializeField] float speed; //移動速度パラメータ
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPositon = transform.localPosition; //初期値の保存
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //変位を計算
+        float z = amlitude + Mathf.Sin(Time.time * speed); //移動量の計算
+
+        //zを変異させたポジションに再設定
+        transform.localPosition = startPositon + new Vector3(0, 0, z);
+        
+    }
+}
